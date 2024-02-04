a99f7209d3343a1f8e6c133aaec5e54f6c59d013
abidt20ahmed <abidahmedmahin21@gmail.com>
2024-02-05 01:25:31 +0600

    crud functionality done

--- app/profile/page.jsx
+++ app/profile/page.jsx
@@ -2,6 +2,7 @@
 
 import Profile from "@components/Profile";
 import { useSession } from "next-auth/react";
+import { ErrorBoundary } from "next/dist/client/components/error-boundary";
 import { useRouter } from "next/navigation";
 import { useEffect, useState } from "react";
 
@@ -22,8 +23,21 @@ const MyProfile = () => {
     const handleEdit = (post) => {
         router.push(`/update-prompt?id=${post._id}`)
     }
-    const handleDelete = (post) => {
+    const handleDelete = async (post) => {
+        const hasConfirmed = confirm("Are you sure you want to delete this prompt?");
 
+        if (hasConfirmed) {
+            try {
+                await fetch(`/api/prompt/${post._id.toString()}`, { method: "DELETE" });
+
+                const filteredPosts = posts.filter(eachPost => eachPost._id !== post._id);
+
+                setPosts(filteredPosts);
+
+            } catch (error) {
+                console.log(ErrorBoundary)
+            }
+        }
     }
 
     useEffect(() => {
